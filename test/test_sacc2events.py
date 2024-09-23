b1c45f3d2ccacd7033c89524d9685dfa1c11e541
Declan Valters <d.valters@epcc.ed.ac.uk>
2024-09-23 10:29:55 +0100

    Linting again!

--- test/test_sacc2events.py
+++ test/test_sacc2events.py
@@ -8,7 +8,7 @@ def test_convert_sacc_to_workload():
     """Test"""
 
     # Set up
-    input_row = "1003,756,21,1703688808,1703987306,1704129658,142352,COMPLETED"
+    # input_row = "1003,756,21,1703688808,1703987306,1704129658,142352,COMPLETED"
     first_submit_time = 1703688806
     input_row_dict = {'JobID': '1003',
                         'NCPUS': '756',
@@ -26,7 +26,9 @@ def test_convert_sacc_to_workload():
     # Since the uid is generated with a random digit, we can't plainly
     # assert that the strings match, but we can use a regex to check for
     # a digit in a certin place (i.e. after UID)
-    assert re.match(r"-dt 2 -e submit_batch_job \| -J jobid_1003 -sim-walltime 142352 --uid=user\d+ -t 00:01:00 -n 21 --ntasks-per-node=36 -A account1 -p normal -q normal pseudo.job", result)
+    assert re.match(r"-dt 2 -e submit_batch_job \| "
+                    r"-J jobid_1003 -sim-walltime 142352 --uid=user\d+ -t 00:01:00 -n 21 "
+                    r"--ntasks-per-node=36 -A account1 -p normal -q normal pseudo.job", result)
     # Assert
     #assert result == output_row
 
@@ -38,14 +40,3 @@ def test_get_dtime_first():
 
     dtime = _get_dtime(input_submit, first_start_time)
     assert dtime == 0
-
-
-def test_get_dtime_first():
-    """Return the start time since simulator start for the
-    job submission"""
-    first_start_time = 1703688808
-    input_submit = 1703688908
-
-    dtime = _get_dtime(input_submit, first_start_time)
-    assert dtime == 100
-
